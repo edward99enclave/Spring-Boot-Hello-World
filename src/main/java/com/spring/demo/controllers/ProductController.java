4d67d054967f810568aeff0771d0751ea02250d8
Edward <edward@mycove.com>
2022-06-22 10:13:08 +0700

    Dev: init - Hello world

--- src/main/java/com/spring/demo/controllers/ProductController.java
+++ src/main/java/com/spring/demo/controllers/ProductController.java
@@ -1,13 +1,15 @@
 package com.spring.demo.controllers;
 
 import com.spring.demo.models.Product;
+import com.spring.demo.models.ResponseObject;
 import com.spring.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController // inform to system that here is class controller
 @RequestMapping(path = "/api/v1/Products") // Routing
@@ -16,11 +18,48 @@ public class ProductController {
     @Autowired
     private ProductRepository repository;
 
-    @GetMapping("") // GET,POST,DELETE MAPPING...
-    // GET: http://localhost:8080/api/v1/Products
-    List<Product> getAllProducts() {
-        return repository.findAll(); // where is data?
-    }
-    // Must save this to Database, use H2 DB (In-memory Database).
+//    @GetMapping("") // GET: http://localhost:8080/api/v1/Products
+//    ResponseEntity<ResponseObject> findAll() {
+//        List<Product> productList = repository.findAll();
+//        return productList.size() != 0 ?
+//            ResponseEntity.status(HttpStatus.OK).body(
+//                    new ResponseObject("ok", "Query list of product successfully", productList)
+//            )
+//            :
+//            ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+//                new ResponseObject("false", "Query list of product Failed", "")
+//            );
+//    };
 
+    @GetMapping("") // GET: http://localhost:8080/api/v1/Products/{id}
+//    ResponseEntity<ResponseObject> findById(@PathVariable Long id) {
+    ResponseEntity<ResponseObject> findById(@RequestParam(value = "id", required = false) Long id) {
+        Optional<Product> foundProduct = repository.findById(id);
+        return foundProduct.isPresent() ?
+            ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("ok", "Query product successfully", foundProduct)
+             )
+            :
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new ResponseObject("false", "Cannot find product with id = " + id, "")
+            );
+    }
+
+    // Insert new Product with POST method
+    // Body: raw, JSON
+    @PostMapping("/create")  // POST: http://localhost:8080/api/v1/Products
+    ResponseEntity<ResponseObject> createProduct(@RequestBody Product newProduct) {
+        // check if the same name
+        // List <Product> foundProduct = repository.findByProductName(newProduct.getProductName().trim());
+        List <Product> foundProduct = repository.findByYear(newProduct.getYear());
+        if (foundProduct.size() > 0) { // exist product
+            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
+                    new ResponseObject("false", "Product Name Existed!", "")
+            );
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Create New Product Successfully", repository.save(newProduct))
+        );
+    }
 }
--- src/main/java/com/spring/demo/repositories/ProductRepository.java
+++ src/main/java/com/spring/demo/repositories/ProductRepository.java
@@ -3,8 +3,11 @@ package com.spring.demo.repositories;
 import com.spring.demo.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // Repositories:
 // Place that save a lost of get data func (memory, file, database,...).
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    List<Product> findByProductName(String productName);
+    List<Product> findByYear(Integer year);
 }
